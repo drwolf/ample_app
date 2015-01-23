967e880aa2f72301fd5d1e3ab5f4096d5163929d
Robert Wolf <derrobertwolf@gmx.de>
2015-01-23 07:53:31 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmpleApp::Application.config.secret_token = '20086f9860cf2dc92d3adbf24a2f5459ff3cf1dada2be27226f1d6c176d7d3ce21959deab69b63921f995fa4178048568458bb112fa9e3b16aa92e432412f6f5'
