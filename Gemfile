7bc1527af9b0d1f6a7c7507a7134142d664d07ea
Robert Wolf <derrobertwolf@gmx.de>
2015-06-04 17:09:51 +0200

    Finish user edit update index destroy actions

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ ruby '1.9.3'
 gem 'rails_12factor'
 gem 'bootstrap-sass', '2.1'
 gem 'bcrypt-ruby', '3.0.1'  
+gem 'faker', '1.0.1'
+gem 'will_paginate', '3.0.3'
+gem 'bootstrap-will_paginate', '0.0.6'
 
 
 group :development, :test do
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,13 @@
 module UsersHelper
-# Returns the Gravatar (http://gravatar.com/) for the given user.
-  def gravatar_for(user, options = { size: 140 })
+ # Returns the Gravatar for the given user.
+  def gravatar_for(user, options = { size: 142 } )    
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
-  end
+    size = options[:size]       
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"   
+    image_tag(gravatar_url, size: size, alt: user.name, class: "gravatar")   
+  end 
 end
+
+#?d=http%3A%2F%2Fexample.com%2Fimages%2Favatar.jpg
+
+
